b20631beb3faaece0120271699a7f379e9a8b317
Policepenguin13 <talia.bruckard@gmail.com>
2025-09-24 18:25:41 +1000

    flower minigame partly done
    finish flower minigame

---
+++ fera/Scripts/flower.gd
@@ -0,0 +1,82 @@
+extends Area2D
+
+@export var waypoints: Array[Node2D]
+
+var from
+
+signal PlusOneFlower
+
+func _ready():
+	# RunAway(self)
+	self.body_entered.connect(BodyEnter)
+	self.body_exited.connect(BodyExit)
+	var target = waypoints.pick_random()
+	# print(str(self.name) + "'s target is " + str(target.name))
+	var offsetX = randf_range(-56.0, 56.0)
+	var offsetY = randf_range(-56.0, 56.0)
+	# print(str(self.name) + " offset X is " + str(offsetX))
+	# print(str(self.name) + " offset Y is " + str(offsetY))
+	self.position.x = target.position.x + offsetX
+	self.position.y = target.position.y + offsetY
+
+func _process(_delta):
+	if self.has_overlapping_areas() or self.has_overlapping_bodies():
+		# print(self.name + " found overlapping bodies, running away!!")
+		if has_overlapping_areas():
+			from = get_overlapping_areas().pick_random()
+		elif has_overlapping_bodies():
+			from = get_overlapping_bodies().pick_random()
+		RunAway()
+
+func RunAway():
+	# print(str(waypoints))
+	# print(str(from))
+	var target
+	# print(str(self.name) + "'s target is " + str(target.name))
+	var offsetX = randf_range(-56.0, 56.0)
+	var offsetY = randf_range(-56.0, 56.0)
+	if randi_range(0,100) >= 50:
+		target = waypoints.pick_random()
+	# print(str(self.name) + "'s target is " + str(target.name))
+	else:
+		target = from
+		var temp = randi_range(1,4)
+		# print(self.name + "'s temp = " + str(temp))
+		if temp == 1:
+			offsetX = 30
+			offsetY = 30
+		elif temp == 2:
+			offsetX = -30
+			offsetY = 30
+		elif temp == 3:
+			offsetX = 30
+			offsetY = -30
+		else:
+			offsetX = -30
+			offsetY = -30
+	# print(str(self.name) + " offset X is " + str(offsetX))
+	# print(str(self.name) + " offset Y is " + str(offsetY))
+	self.position.x = target.position.x + offsetX
+	self.position.y = target.position.y + offsetY
+	
+	if from != null:
+		if from.is_in_group("flower"):
+			# print(str(self.name)+" ran away from another flower ("+str(from.name)+")")
+			pass
+		if from == $"../../TileMapLayer":
+			# print(str(self.name)+ " ran away from the tile map")
+			pass
+		if from == $"../../Gatherer":
+			PlusOneFlower.emit()
+		else:
+			# print(str(self.name)+" ran away from something that wasn't a flower or the tilemap, " + str(from.name))
+			pass
+
+
+func BodyEnter(body):
+	# print(str(self.name)+"'s body was entered by " + str(body.name))
+	from = body
+	RunAway()
+
+func BodyExit(_body):
+	from = null
--- fera/Scripts/flower_minigame.gd
+++ fera/Scripts/flower_minigame.gd
@@ -1,11 +1,26 @@
 extends Node2D
 
+# @onready var scene = preload("res://Scenes/flower.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	for child in $flowers.get_children():
+		child.PlusOneFlower.connect(PlusOne)
+		# print("connected " + str(child.name) + "'s PlusOneFlower to PlusOne")
+	$"+1".hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
+
+func PlusOne():
+	if $wait.is_stopped():
+		# print("+1!")
+		$"+1".position = $Gatherer.position
+		$"+1".show()
+		$wait.start()
+		await $wait.timeout
+		$"+1".hide()
+	else:
+		# print("wait is stopped")
+		pass
